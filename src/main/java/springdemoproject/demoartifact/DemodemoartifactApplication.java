c3531c44805f9323d8b2d746190eb3be81b307e0
First Last <ruchisinha648gm@ip-172-31-16-212.ec2.internal>
2022-03-13 12:56:40 +0000

    adding files

---
+++ src/main/java/springdemoproject/demoartifact/DemodemoartifactApplication.java
@@ -0,0 +1,13 @@
+package springdemoproject.demoartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodemoartifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodemoartifactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springdemoproject/demoartifact/DemodemoartifactApplicationTests.java
@@ -0,0 +1,13 @@
+package springdemoproject.demoartifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodemoartifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
